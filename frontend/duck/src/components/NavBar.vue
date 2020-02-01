b39d330c5451dd46911dd9fe2ef206e49f6883eb
Dulcie <dulciejackson@gmail.com>
2020-02-01 17:34:16 +0000

    Duckicon

--- frontend/duck/src/components/NavBar.vue
+++ frontend/duck/src/components/NavBar.vue
@@ -7,26 +7,18 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Get Started</b-nav-item>
+        <b-nav-item href="/">Get Started</b-nav-item>
         <b-nav-item href="#">About</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
 
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
         <b-nav-item-dropdown right>
           <!-- Using 'button-content' slot -->
           <template v-slot:button-content>
             <em>User</em>
           </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
--- frontend/duck/src/views/AllPlaylists.vue
+++ frontend/duck/src/views/AllPlaylists.vue
@@ -2,19 +2,34 @@
   <div class="playlists">
     <h1>Your Playlists</h1>
     <ul class="playlist-container">
-        <PlaylistPreview title="Sample Playlist" description="A description" image="https://image.flaticon.com/icons/svg/1753/1753344.svg"/>
-        <PlaylistPreview title="Another sample playlist" description="Now these can be used in list rendering" image="https://image.flaticon.com/icons/svg/1753/1753344.svg"/>
+        <PlaylistPreview class="playlist" title="Sample Playlist" description="A description" image="https://image.flaticon.com/icons/svg/2284/2284983.svg"/>
+        <PlaylistPreview class="playlist" title="Another sample playlist" description="Now these can be used in list rendering" image="https://image.flaticon.com/icons/svg/2284/2284983.svg"/>
     </ul>
   </div>
 </template>
 
 <script>
 import PlaylistPreview from '../components/PlaylistPreview.vue';
+import axios from 'axios';
 
 export default {
   name: 'playlists',
   components: {
       PlaylistPreview
+  },
+  data() {
+    return {
+      playlists: [],
+    }
+  },
+  created() {
+    axios.get('http://localhost:8000/playlists')
+    .then(response => {
+      this.playlists = response.data;
+    })
+    .catch(e => {
+      console.log(e);
+    })
   }
 }
 </script>
@@ -25,5 +40,9 @@ export default {
     margin-left: 30%;
     margin-top: 5%;
     text-align: left;
+}
+
+.playlist {
+    margin: 1rem 0 1rem 0;
 }
 </style>
