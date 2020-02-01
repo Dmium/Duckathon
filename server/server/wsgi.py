80b3d38cb652923f62ff6ce660a02cc81ed6453e
Dom Hall <dominiczy.hall@gmail.com>
2020-02-01 14:03:15 +0000

    Generic env vars

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -13,8 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from . import keys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-os.environ.setdefault('SPOTIPY_CLIENT_ID', keys.SPOTIPY_CLIENT_ID)
-os.environ.setdefault('SPOTIPY_CLIENT_SECRET', keys.SPOTIPY_CLIENT_SECRET)
-os.environ.setdefault('SPOTIPY_REDIRECT_URI', keys.SPOTIPY_REDIRECT_URI)
+for envvar in keys.envvars:
+    os.environ.setdefault(envvar[0], envvar[1])
 
 application = get_wsgi_application()
