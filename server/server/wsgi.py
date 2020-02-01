f10392feecdd9bdacb184f9d395592e2cae77f44
Dom Hall <dominiczy.hall@gmail.com>
2020-02-01 13:57:24 +0000

    Keys in env variables

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from . import keys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+os.environ.setdefault('SPOTIPY_CLIENT_ID', keys.SPOTIPY_CLIENT_ID)
+os.environ.setdefault('SPOTIPY_CLIENT_SECRET', keys.SPOTIPY_CLIENT_SECRET)
+os.environ.setdefault('SPOTIPY_REDIRECT_URI', keys.SPOTIPY_REDIRECT_URI)
 
 application = get_wsgi_application()
