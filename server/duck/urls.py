8fc1984b29d9842393887c833d344c21e269dd91
Julia Meister <juliameister@users.noreply.github.com>
2020-02-01 21:54:54 +0000

    Cleaned endpoint names

--- server/duck/urls.py
+++ server/duck/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
 
+    path('playlist/<id>', views.playlist, name='playlist'),
     path('playlists', views.playlists, name='playlists'),
     path('playlists/add', views.add_to_playlists, name='playlists_add'),
-    path('playlists/<id>', views.playlist, name='playlist'),
+    path('playlists/create', views.create_playlist, name='playlist_create'),
     path('playlists/merge', views.merge_playlists, name='playlists_merge'),
 ]
