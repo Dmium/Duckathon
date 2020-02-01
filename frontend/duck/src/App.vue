df860864cd553f89f2404c616f04172025881700
Dulcie <dulciejackson@gmail.com>
2020-02-01 12:42:00 +0000

    Adds navbar

--- frontend/duck/src/App.vue
+++ frontend/duck/src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <NavBar/>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,10 +25,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
 }
 
 #nav a {
