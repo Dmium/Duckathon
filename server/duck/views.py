585578cb9c916af9338386a9f6193cde25909df1
Julia Meister <juliameister@users.noreply.github.com>
2020-02-02 10:43:25 +0000

    Un-hardcoded playlist/add_albums

--- server/duck/views.py
+++ server/duck/views.py
@@ -54,9 +54,9 @@ def logout(request):
 def add_albums_to_playlist(request):
     """Add all tracks from the provided albums to a specified playlist."""
 
-    # Fake input
-    playlist_id = "6JW6em51gITQVoMZpcjVHL"
-    album_ids = ["6P9CHi6Dx26YWHeE3aTZiz", "7gTFf2EWBONpjKVjpWbs5p"]
+    data = json.load(request)
+    playlist_id = data['playlist_id']
+    album_ids = data['album_ids']
 
     user_id, _, sp = get_auth(request)
 
@@ -191,9 +191,6 @@ def create_playlist(request):
 
     # set public and collaborative
     # sp.user_playlist_change_details(user_id, playlist['id'], public=False, collaborative=True)
-
-    # add image
-    # sp.playlist_upload_cover_image(playlist['id'], "image_b64")
 
     return JsonResponse(playlist)
 
